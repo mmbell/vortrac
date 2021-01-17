ce3d419c0ef6e0163391dff01f7cc9e48e7d9c02
mike-dixon <dixon@ucar.edu>
2021-01-17 16:10:51 -0700

    cleaning up cmake for elle final release

--- cmake/modules/lrose-config.cmake
+++ cmake/modules/lrose-config.cmake
@@ -1,20 +1,20 @@
 # User can tell us where to look
 
-set ( LROSE_PREFIX $ENV{LROSE_INSTALL_DIR})
+set(LROSE_PREFIX $ENV{LROSE_INSTALL_DIR})
 
 # If not, use the default location
-if (NOT LROSE_PREFIX)
-  set (LROSE_PREFIX "/usr/local/lrose")
-endif ( NOT LROSE_PREFIX )
+if(NOT LROSE_PREFIX)
+  set(LROSE_PREFIX "/usr/local/lrose")
+endif(NOT LROSE_PREFIX)
 
-set ( LROSE_LIBRARIES kd tdrp Radx Ncxx toolsa netcdf hdf5_cpp hdf5 z )
+set(LROSE_LIBRARIES kd tdrp Radx Ncxx toolsa netcdf hdf5_cpp hdf5 z)
 
 # Add a custom generator for TDRP Params.cc and Params.hh files
 # from their associated paramdef.<app> file
 
-find_program ( TDRP_EXECUTABLE tdrp_gen PATHS ${LROSE_BIN_DIR} /usr/local/bin )
+find_program(TDRP_EXECUTABLE tdrp_gen PATHS ${LROSE_BIN_DIR} /usr/local/bin)
 
-add_custom_command (
+add_custom_command(
   OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Params.hh ${CMAKE_CURRENT_SOURCE_DIR}/Params.cc
   DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/paramdef.${PROJECT_NAME}
   COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} && ${TDRP_EXECUTABLE}
@@ -26,8 +26,7 @@ add_custom_command (
   )
 
 
-message ( STATUS "lrose-config: CMAKE_INSTALL_PREFIX: " ${CMAKE_INSTALL_PREFIX} )
-message ( STATUS "lrose-config: LROSE_PREFIX: " ${LROSE_PREFIX} )
-message ( STATUS "lrose-config: LROSE_LIBRARIES: " ${LROSE_LIBRARIES} )
-message ( STATUS "lrose-config: TDRP_EXECUTABLE: " ${TDRP_EXECUTABLE} )
+message(STATUS "lrose-config: CMAKE_INSTALL_PREFIX: " ${CMAKE_INSTALL_PREFIX})
+message(STATUS "lrose-config: LROSE_PREFIX: " ${LROSE_PREFIX})
+message(STATUS "lrose-config: TDRP_EXECUTABLE: " ${TDRP_EXECUTABLE})
 
